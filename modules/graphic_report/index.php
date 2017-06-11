788ee2c8625522ae46a55753c1c9b2ee56305e02
Nicolas Gudino <asternic@gmail.com>
2017-06-11 12:31:49 -0400

    Upudate attribution comments leaving original copyright and GPL license in place

--- modules/graphic_report/index.php
+++ modules/graphic_report/index.php
@@ -17,7 +17,6 @@
   | the License for the specific language governing rights and           |
   | limitations under the License.                                       |
   +----------------------------------------------------------------------+
-  | The Original Code is: Elastix Open Source.                           |
   | The Initial Developer of the Original Code is PaloSanto Solutions    |
   +----------------------------------------------------------------------+
   $Id: default.conf.php,v 1.1 2008-09-01 10:09:57 jjvega Exp $ 
